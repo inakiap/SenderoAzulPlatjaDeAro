787d1b7f30bb6f228ec3c680e846c0ef634d843d
inakiap <inakiap@gmail.com>
2014-03-27 09:52:55 +0100

    Revert "back"
    This reverts commit 6193bf4456b6c501c4153402eec9a46efca6b7ca.

--- js/back.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("backbutton", function(e){
-    if($.mobile.activePage.is('index.html')){
-        e.preventDefault();
-        navigator.app.exitApp();
-    }
-    else {
-        navigator.app.backHistory()
-    }
-}, false);
